83249261db7e2955c2f0f070a06599eccc392788
MsDacia <daciaj@yahoo.com>
2017-08-31 21:23:37 -0500

    About page stuff

--- projects/vue/src/routes/about.vue
+++ projects/vue/src/routes/about.vue
@@ -4,15 +4,27 @@
 			<h1>{{content.about.title}}</h1>
 		</div>
 		<div class="fourteen wide column">
-			<h2>{{content.about.subtitle}}</h2>
-			<p v-html="content.about.copy"></p>
-
-			<h2>{{content.about.subtitle2}}</h2>
-			<p v-html="content.about.copy2"></p>
-
-			<h2>{{content.about.subtitle3}}</h2>
-			<p v-html="content.about.copy3"></p>
-			<p v-html="content.about.copy4"></p>
+			<div class="ui accordion">
+				<div class="active title">
+					<h2>{{content.about.subtitle}}</h2>
+				</div>
+				<div class="active content">
+					<p v-html="content.about.copy"></p>
+				</div>
+				<div class="title">
+					<h2>{{content.about.subtitle2}}</h2>
+				</div>
+				<div class="content">
+					<p v-html="content.about.copy2"></p>
+				</div>
+				<div class="title">
+					<h2>{{content.about.subtitle3}}</h2>
+				</div>
+				<div class="content">
+					<p v-html="content.about.copy3"></p>
+					<p v-html="content.about.copy4"></p>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -24,6 +36,9 @@
 			return {
 				content: require('../json/static.en-us.json')
 			}
+		},
+		mounted() {
+			$('.ui.accordion').accordion()
 		}
 	}
 
