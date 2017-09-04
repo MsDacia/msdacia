673167077e4cb6a38d16986c975038999ecc7a71
MsDacia <daciaj@yahoo.com>
2017-09-04 09:47:56 -0500

    WIP Portfolio

--- projects/vue/src/routes/portfolio.vue
+++ projects/vue/src/routes/portfolio.vue
@@ -1,15 +1,22 @@
 <template>
-	<div>
-		<h1>{{content.projects.title}}</h1>
+	<div class="ui grid">
+		<div class="two wide column">
+			<h1>{{content.projects.title}}</h1>
+		</div>
+		<div class="fourteen wide column">	
+			W.I.P.
 
-		<div class="content">
-			<p v-html="content.projects.copy"></p>
-			<h2>{{content.projects.subtitle}}</h2>
-			<ul>
-				<li v-for="link in content.projects.list" :key="link.id">
-					<p><a :href="link.url" target="_blank"><i :class="link.icon"></i>{{link.title}}</a></p>
-				</li>
-			</ul>
+			<!-- <h1>{{content.projects.title}}</h1>
+
+			<div class="content">
+				<p v-html="content.projects.copy"></p>
+				<h2>{{content.projects.subtitle}}</h2>
+				<ul>
+					<li v-for="link in content.projects.list" :key="link.id">
+						<p><a :href="link.url" target="_blank"><i :class="link.icon"></i>{{link.title}}</a></p>
+					</li>
+				</ul>
+			</div> -->
 		</div>
 	</div>
 </template>
