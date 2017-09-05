22577fcbf10ec94e630b80f31ad3da7f01b08e1d
MsDacia <daciaj@yahoo.com>
2017-09-04 22:03:15 -0500

    Images updates and json updates

--- projects/vue/src/routes/portfolio.vue
+++ projects/vue/src/routes/portfolio.vue
@@ -10,11 +10,31 @@
 
 			<h2>{{content.projects.subtitle}}</h2>
 
-			<ul>
-				<li v-for="link in content.projects.list" :key="link.id">
-					<p><a :href="link.url" target="_blank"><i :class="link.icon"></i>{{link.title}}</a></p>
-				</li>
-			</ul>
+			<div class="ui list">
+				<div class="item" v-for="link in content.projects.list" :key="link.id">
+					<div class="content">
+						<a :href="link.url" target="_blank"><i :class="link.icon"></i>{{link.title}}</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="ui container">
+				<div class="timeline">
+					<div class="item" :data-text="project.title" v-for="project in content.projects.project" :key="project.id">
+						<div class="content">
+							<img class="image" :src="'assets/images/projects/' + project.image" />
+							<h2 class="title" v-html="project.year"></h2>
+							<div class="ui tag labels">
+								<a class="ui label" v-for="tag in project.tags" :key="tag.id">
+									{{tag}}
+								</a>
+							</div>
+							<h3>{{project.client}}</h3>
+							<h4>{{project.timeline}}</h4>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
