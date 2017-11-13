75ace81f37d8014526cffad4ebc372889d1f6149
MsDacia <daciaj@yahoo.com>
2017-11-12 21:23:47 -0600

    Added blog components

---
+++ projects/vue/src/components/blog-nav.vue
@@ -0,0 +1,15 @@
+<template>
+	<h3>Blog Nav</h3>
+</template>
+
+<script>
+
+	export default {
+		data: function () {
+			return {
+				content: require('../json/static.en-us.json')
+			}
+		}
+	}
+
+</script>
